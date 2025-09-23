36a0d173483ee00dc4b57953eefaa549d0e97a93
daviiid24 <christiand.loperac@uqvirtual.edu.co>
2025-09-23 16:18:33 -0500

    c

--- co.edu.uniquindio.universidad/src/main/java/co/edu/uniquindio/universidad/Main.java
+++ co.edu.uniquindio.universidad/src/main/java/co/edu/uniquindio/universidad/Main.java
@@ -1,66 +1,142 @@
-package co.edu.uniquindio.universidad;
-
 import co.edu.uniquindio.universidad.model.*;
+
+import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args) {
         Universidad universidad = inicializaDatos();
 
-        Estudiante estudiante = universidad.getListaEstudiantes().get(0);
-        estudiante.getOwnedByUniversidad();
+        //Estudiante estudiante = universidad.getListaEstudiantes().get(0);
+        //estudiante.getOwnedByUniversidad();
 
-        calcularDefinitivaEstudiante(estudiante1, docente);
-        calcularPromedioCurso(estudiante1, estudiante2, estudiante3, docente);
-        calcularPromedioEdad(estudiante1, estudiante2, estudiante3, docente);
-        calcularPromedioNota1(estudiante1, estudiante2, estudiante3, docente);
-        calcularNotaMayorCurso(estudiante1, estudiante2, estudiante3, docente);
-        calcularNotaMenorCurso(estudiante1, estudiante2, estudiante3, docente);
-        aprobacionCurso(estudiante1, estudiante2, estudiante3, docente);
-        porcentajeAprobados(estudiante1, estudiante2, estudiante3, docente);
-        porcentajeReprobados(estudiante1, estudiante2, estudiante3, docente);
-        obtenerEstudianteNotasMayor4(estudiante1,  estudiante2, estudiante3, docente);
-        obtenerNotaMaxima(estudiante1,  estudiante2, estudiante3, docente);
+                int opcionSeleccionada;
 
+                do {
+                    mostrarMenu();
+                    opcionSeleccionada = leerEntero("Ingrese una opción: ");
+
+                    switch (opcionSeleccionada) {
+                        case 1:
+                            String respuesta = crearEstudiante();
+                            System.out.println(respuesta);
+                            break;
+                        case 2:
+                            String respuesta1 = crearDocente();
+                            System.out.println(respuesta1);
+                            break;
+                        case 3:
+                            String respuesta2 = crearCurso();
+                            System.out.println(respuesta2);
+                            break;
+                        case 4:
+                            System.out.println("Saliendo de la aplicación...");
+                            break;
+                        default:
+                            System.out.println("Opción no válida, intente de nuevo.");
+                    }
+                } while (opcionSeleccionada != 4);
+            }
+
+            //
+       //calcularDefinitivaEstudiante(estudiante1, docente);
+//calcularPromedioCurso(estudiante1, estudiante2, estudiante3, docente);
+//calcularPromedioEdad(estudiante1, estudiante2, estudiante3, docente);
+//calcularPromedioNota1(estudiante1, estudiante2, estudiante3, docente);
+//calcularNotaMayorCurso(estudiante1, estudiante2, estudiante3, docente);
+//calcularNotaMenorCurso(estudiante1, estudiante2, estudiante3, docente);
+//aprobacionCurso(estudiante1, estudiante2, estudiante3, docente);
+//porcentajeAprobados(estudiante1, estudiante2, estudiante3, docente);
+//porcentajeReprobados(estudiante1, estudiante2, estudiante3, docente);
+//obtenerEstudianteNotasMayor4(estudiante1,estudiante2, estudiante3, docente);
+//obtenerNotaMaxima(estudiante1,  estudiante2, estudiante3, docente);
+//
     }
 
     private static Universidad inicializaDatos() {
-        Universidad universidad = inicializaDatos();
-
-        Estudiante estudiante1 = new Estudiante();
-        estudiante1.setNombre("Raul");
-        estudiante1.setEdad(19);
-        estudiante1.setCorreo("ujfe");
-        estudiante1.setSemestre(8);
-        estudiante1.setNota1(4.0);
-        estudiante1.setNota2(3.5);
-        estudiante1.setNota3(4.5);
-        Estudiante estudiante2 = new Estudiante();
-        estudiante2.setNombre("Rodolfo");
-        estudiante2.setEdad(17);
-        estudiante2.setCorreo("jberjgk");
-        estudiante2.setSemestre(9);
-        estudiante2.setNota1(5.0);
-        estudiante2.setNota2(4.9);
-        estudiante2.setNota3(4.7);
-        Estudiante estudiante3 = new Estudiante();
-        estudiante3.setNombre("Ana");
-        estudiante3.setEdad(20);
-        estudiante3.setCorreo("vjwi");
-        estudiante3.setSemestre(9);
-        estudiante3.setNota1(4.5);
-        estudiante3.setNota2(3.0);
-        estudiante3.setNota3(4.5);
-        Docente docente= new Docente();
-        docente.setNombre("Pedro");
-        docente.setEdad(35);
-        docente.setCorreo("sbvi");
-        Rector rector = new Rector();
-        rector.setNombre("Rodolfo");
-        rector.setApellido("Aifi");
+        Universidad universidad = new Universidad("Uniquindio");
+        return universidad;
     }
 
-    public static void calcularDefinitivaEstudiante(Estudiante estudiante, Docente docente){
-        double definitiva=docente.calcularDefinitivaEstudiante(estudiante.getNota1(), estudiante.getNota2(), estudiante.getNota3());
+private static void mostrarMenu() {
+    System.out.println("\n===== MENÚ UNIVERSIDAD =====");
+    System.out.println("1 - Crear estudiante");
+    System.out.println("2 - Crear docente");
+    System.out.println("3 - Crear curso");
+    System.out.println("4 - Salir");
+}
+
+private static int leerEntero(String mensaje) {
+    int dato = 0;
+    String captura = "";
+    System.out.println(mensaje);
+    Scanner teclado = new Scanner(System.in);
+    captura = teclado.nextLine();
+    dato = Integer.parseInt(captura);
+    return dato;
+}
+public static String leerStringConsola(String mensaje)
+{
+    String captura="";
+    System.out.println(mensaje);
+    Scanner teclado = new Scanner(System.in);
+    captura = teclado.nextLine();
+    return captura;
+}
+public static double leerDoubleConsola(String mensaje)
+{
+    double dato=0;
+    String captura="";
+    System.out.println(mensaje);
+    Scanner teclado = new Scanner(System.in);
+    captura = teclado.nextLine();
+    dato=Double.parseDouble(captura);
+    return dato;
+}
+
+
+public static String crearEstudiante() {
+    String nombre = leerStringConsola("Ingrese el nombre del estudiante: ");
+    int edad = leerEntero("Ingrese la edad del estudiante: ");
+    String correo = leerStringConsola("Ingrese el correo del estudiante: ");
+    int semestre = leerEntero("Ingrese el semestre del estudiante: ");
+    double nota1 = leerDoubleConsola("Ingrese la nota 1: ");
+    double nota2 = leerDoubleConsola("Ingrese la nota 2: ");
+    double nota3 = leerDoubleConsola("Ingrese la nota 3: ");
+
+    Estudiante estudiante1 = new Estudiante(nombre, edad, correo, semestre, nota1, nota2, nota3);
+
+    return estudiante1.toString();
+}
+
+public static String crearDocente() {
+    String nombre = leerStringConsola("Ingrese el nombre del docente: ");
+    int edad = leerEntero("Ingrese la edad del docente: ");
+    String correo = leerStringConsola("Ingrese el correo del docente: ");
+
+    Docente docente1 = new Docente(nombre, edad, correo);
+
+    return docente1.toString();
+}
+
+public static String crearCurso() {
+    String nombre = leerStringConsola("Ingrese el nombre del curso: ");
+    String semestre = leerStringConsola("Ingrese el semestre del curso: ");
+    String grupo = leerStringConsola("Ingrese el grupo del curso: ");
+    double creditos = leerDoubleConsola("Ingrese los créditos del curso: ");
+    String jornada = leerStringConsola("Ingrese la jornada del curso: ");
+
+    Curso curso1 = new Curso(nombre, semestre, grupo, creditos, jornada, null);
+
+    return curso1.toString();
+}
+
+void main() {
+}
+
+/*public static void calcularDefinitivaEstudiante(Estudiante estudiante, Docente docente){
+
+    double definitiva=docente.calcularDefinitivaEstudiante(estudiante.getNota1(), estudiante.getNota2(), estudiante.getNota3());
         System.out.println("La definitiva es: "+definitiva);
     }
     public static void calcularPromedioCurso(Estudiante estudiante1, Estudiante estudiante2, Estudiante estudiante3, Docente docente) {
@@ -126,5 +202,4 @@ public class Main {
         double def3 = docente.calcularDefinitivaEstudiante(estudiante3.getNota1(), estudiante3.getNota2(), estudiante3.getNota3());
         double notaMaxima=docente.obtenerNotaMaxima(def1, def2, def3);
         System.out.println("La nota maxima es de: "+notaMaxima);
-    }
-}
+    }*/
